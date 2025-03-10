5c5a7fc5174dc8ac1f13d9da0c70312a2d25c724
Mohamed Shadhan <shadhanmoahmed13@gmail.com>
2025-03-10 19:46:12 +0200

    login/out & getMe added

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
 import connectMongoDB from "./db/connectMongoBD.js";
@@ -11,6 +12,8 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // This is a built-in middleware function in Express. It parses incoming requests with JSON payloads and is based on body-parser.
 app.use(express.urlencoded({ extended: true })); // This is a built-in middleware function in Express. It parses incoming requests with urlencoded payloads and is based on body-parser.
+
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
